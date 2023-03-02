ff5250a6513d45b7b37cb07dc49279060a776eba
Alexandru Zaharia <alzaharia@deloittece.com>
2023-03-02 15:31:45 +0200

    update

--- src/main/java/org/lumaSite/base/BasePage.java
+++ src/main/java/org/lumaSite/base/BasePage.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class BasePage {
-
     private static WebDriver driver;
     private static WebDriverWait wait;
 
